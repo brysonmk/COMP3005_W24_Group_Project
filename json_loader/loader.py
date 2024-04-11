b5403747c41dc17f1ce0f4eff06f8a3b0f0ad246
Saeid El-Saadi <121737118+SaeidEl-Saadi@users.noreply.github.com>
2024-04-11 17:45:14 -0400

    Added matches table data mapping and loader.sql to recreate tables

--- json_loader/loader.py
+++ json_loader/loader.py
@@ -8,6 +8,10 @@ competitionsTarget = ['Premier League', 'La Liga']
 conn = psycopg.connect(dbname='project_database', user='postgres', password='1234', host='localhost', port='5432')
 cur = conn.cursor()
 
+#table creation
+cur.execute(open(os.getcwd() + '\json_loader\loader.sql', 'r').read())
+conn.commit()
+
 #load data from competitions
 competitions = json.load(open(os.getcwd() + '\json_loader\data\competitions.json', 'r'))
 
@@ -57,8 +61,22 @@ for file in os.listdir(matchesRoot):
                 matchIds.append(obj['match_id'])
 
 #load match data into database
-#for obj in matchObjects:
-#    print(obj)
+seasonsList = ['2020/2021', '2019/2020', '2018/2019', '2003/2004']
+for obj in matchObjects:
+    i = seasonsList.index(obj['season']['season_name'])
+    if 'referee' not in obj:
+        obj['referee'] = {'id': None, 'name': None, 'country': {'id': None, 'name': None}}
+    if 'managers' not in obj['home_team']:
+        obj['home_team']['managers'] = [{'id': None, 'name': None, 'nickname': None, 'dob': None, 'country': {'id': None, 'name': None}}]
+    if 'manager' not in obj['away_team']:
+        obj['away_team']['managers'] = [{'id': None, 'name': None, 'nickname': None, 'dob': None, 'country': {'id': None, 'name': None}}]
+    if 'stadium' not in obj:
+        obj['stadium'] = {'id': None, 'name': None, 'country': {'id': None, 'name': None}}
+    cur.execute(
+        "INSERT INTO Matches (match_id, id, competition_id, competition_name, country_name, season_id, season_name, match_date, kick_off, stadium_id, stadium_name, stadium_country, referee_id, referee_name, referee_country_id, referee_country_name, home_team_id, home_team_name, home_team_gender, home_team_manager_id, home_team_manager_name, home_team_manager_nickname, home_team_manager_dob, home_team_manager_country_id, home_team_manager_country_name, home_team_group, home_team_country_id, home_team_country_name, away_team_id, away_team_name, away_team_gender, away_team_manager_id, away_team_manager_name, away_team_manager_nickname, away_team_manager_dob, away_team_manager_country_id, away_team_manager_country_name, away_team_group, away_team_country_id, away_team_country_name, home_score, away_score, match_status, match_week, competition_stage_id, competition_stage_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (obj['match_id'], i, obj['competition']['competition_id'], obj['competition']['competition_name'], obj['competition']['country_name'], obj['season']['season_id'], obj['season']['season_name'], obj['match_date'], obj['kick_off'], obj['stadium']['id'], obj['stadium']['name'], obj['stadium']['country']['name'], obj['referee']['id'], obj['referee']['name'], obj['referee']['country']['id'], obj['referee']['country']['name'], obj['home_team']['home_team_id'], obj['home_team']['home_team_name'], obj['home_team']['home_team_gender'], obj['home_team']['managers'][0]['id'], obj['home_team']['managers'][0]['name'], obj['home_team']['managers'][0]['nickname'], obj['home_team']['managers'][0]['dob'], obj['home_team']['managers'][0]['country']['id'], obj['home_team']['managers'][0]['country']['name'], obj['home_team']['home_team_group'], obj['home_team']['country']['id'], obj['home_team']['country']['name'], obj['away_team']['away_team_id'], obj['away_team']['away_team_name'], obj['away_team']['away_team_gender'], obj['away_team']['managers'][0]['id'], obj['away_team']['managers'][0]['name'], obj['away_team']['managers'][0]['nickname'], obj['away_team']['managers'][0]['dob'], obj['away_team']['managers'][0]['country']['id'], obj['away_team']['managers'][0]['country']['name'], obj['away_team']['away_team_group'], obj['away_team']['country']['id'], obj['away_team']['country']['name'], obj['home_score'], obj['away_score'], obj['match_status'], obj['match_week'], obj['competition_stage']['id'], obj['competition_stage']['name'])
+    )
+conn.commit()
 
 
 #load data from events
--- json_loader/loader.sql
+++ json_loader/loader.sql
@@ -20,47 +20,48 @@ CREATE TABLE Competitions (
 
 CREATE TABLE Matches (
 	match_id INTEGER PRIMARY KEY,
+	id INTEGER,
 	
 	competition_id INTEGER,
 	competition_name VARCHAR(255),
 	
 	country_name VARCHAR(255),
 	
-	season_id INTEGER UNIQUE,
+	season_id INTEGER,
 	season_name VARCHAR(255),
 	
-	match_date DATE,
-	kick_off TIME,
+	match_date VARCHAR(255),
+	kick_off VARCHAR(255),
 	
-	stadium_id INTEGER UNIQUE,
+	stadium_id INTEGER,
 	stadium_name VARCHAR(255),
 	stadium_country VARCHAR(255),
 	
-	referee_id INTEGER UNIQUE,
+	referee_id INTEGER,
 	referee_name VARCHAR(255),
-	referee_country_id INTEGER UNIQUE,
+	referee_country_id INTEGER,
 	referee_country_name VARCHAR(255),
 	
-	home_team_id INTEGER UNIQUE,
+	home_team_id INTEGER,
 	home_team_name VARCHAR(255),
 	home_team_gender VARCHAR(255),
-	home_team_manager_id INTEGER UNIQUE,
+	home_team_manager_id INTEGER,
 	home_team_manager_name VARCHAR(255),
 	home_team_manager_nickname VARCHAR(255),
-	home_team_manager_dob DATE,
+	home_team_manager_dob VARCHAR(255),
 	home_team_manager_country_id INTEGER,
 	home_team_manager_country_name VARCHAR(255),
 	home_team_group VARCHAR(255),
 	home_team_country_id INTEGER,
 	home_team_country_name VARCHAR(255),
 	
-	away_team_id INTEGER UNIQUE,
+	away_team_id INTEGER,
 	away_team_name VARCHAR(255),
 	away_team_gender VARCHAR(255),
-	away_team_manager_id INTEGER UNIQUE,
+	away_team_manager_id INTEGER,
 	away_team_manager_name VARCHAR(255),
 	away_team_manager_nickname VARCHAR(255),
-	away_team_manager_dob DATE,
+	away_team_manager_dob VARCHAR(255),
 	away_team_manager_country_id INTEGER,
 	away_team_manager_country_name VARCHAR(255),
 	away_team_group VARCHAR(255),
@@ -73,8 +74,8 @@ CREATE TABLE Matches (
 	match_week INTEGER,
 	competition_stage_id INTEGER,
 	competition_stage_name VARCHAR(255),
-	last_updated DATE, 	-- Unable to use DATETIME
+	--last_updated DATE, 	-- Unable to use DATETIME
 	
-	
+	FOREIGN KEY (id) REFERENCES Competitions(id),
 	FOREIGN KEY (season_id) REFERENCES Competitions(season_id)
 );
